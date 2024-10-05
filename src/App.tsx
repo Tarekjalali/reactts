5c8555abd7e30fd9062a51e168ba1326e21fc7c2
Tarekjalali <jlali.tare9@gmail.com>
2024-10-05 17:50:23 +0100

    fd

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './Components/Counter';
+import Greeting from './Components/Greeting';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="ala"/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
